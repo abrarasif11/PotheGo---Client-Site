2078afaf66f4f0f71c2d874e8c4cc6a561703214
abrarasif11 <faasif929@gmail.com>
2025-10-03 23:21:42 +0600

    FAQ section

--- src/Components/Home/FAQ/FAQ.jsx
+++ src/Components/Home/FAQ/FAQ.jsx
@@ -6,35 +6,28 @@ const FAQ = () => {
       <h1 className="text-4xl text-center font-extrabold">
         Frequently Asked Question (FAQ)
       </h1>
-      <div className="text-center">
-        <p className="mt-6">
-          Enhance posture, mobility, and well-being effortlessly with Posture
-          Pro. Achieve proper alignment, reduce
-        </p>
-        <br />
-        <p className="mb-10">pain, and strengthen your body with ease!</p>
-      </div>
+
       {/* FAQ */}
       <div className="collapse collapse-arrow bg-base-100 mt-20 border border-base-300">
         <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title font-semibold">
-          What is TrackMate Parcel Management System?
-        </div>
+        <div className="collapse-title font-semibold">What is PotheGo?</div>
         <div className="collapse-content text-sm">
-          TrackMate is an intelligent parcel booking, tracking, and delivery
-          management system that operates across all 64 districts of Bangladesh,
-          ensuring fast, reliable, and secure deliveries
+          PotheGo is an intelligent parcel booking, tracking, and delivery
+          platform that connects individuals and businesses across all 64
+          districts of Bangladesh. With AI-driven route optimization, real-time
+          tracking, and predictive delivery time estimation, PotheGo ensures
+          safe, fast, and reliable parcel delivery.
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-100 border border-base-300">
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title font-semibold">
-          How does parcel tracking work?
+          How does parcel potheGo work?
         </div>
         <div className="collapse-content text-sm">
           Every parcel gets a unique tracking ID. Customers can enter this ID on
-          the TrackMate platform to view real-time updates — from booking to
-          final delivery.
+          the potheGo platform to view real-time updates — from booking to final
+          delivery.
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-100 border border-base-300">
@@ -45,7 +38,7 @@ const FAQ = () => {
         <div className="collapse-content text-sm">
           You can book a parcel directly through our online platform or at any
           of our district branches. Simply provide sender/receiver details,
-          parcel information, and select your preferred delivery option.
+          parcel information and select your preferred delivery option.
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-100 border mb-20 border-base-300">
@@ -54,8 +47,8 @@ const FAQ = () => {
           Do you cover all districts in Bangladesh?
         </div>
         <div className="collapse-content text-sm">
-          Yes! TrackMate ensures full nationwide coverage with service branches
-          in all 64 districts, making parcel delivery accessible everywhere.
+          Yes! potheGo ensures full nationwide coverage with service branches in
+          all 64 districts, making parcel delivery accessible everywhere.
         </div>
       </div>
     </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "./Banner/Banner";
 import WhyPotheGo from "./WhyPotheGo/WhyPotheGo";
 import PricingSection from "./PricingSection/PricingSection";
 import HowItWorks from "./HowItWorks/HowItWorks";
+import FAQ from "./FAQ/FAQ";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <WhyPotheGo />
       <PricingSection />
       <HowItWorks />
+      <FAQ />
     </div>
   );
 };
