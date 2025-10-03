c92fe27ff57bbd85766a9e0e603a867ac9d41e7b
abrarasif11 <faasif929@gmail.com>
2025-10-03 19:25:20 +0600

    footer completed

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+
+const Navbar = () => {
+  //   const { user, logOut } = useAuth();
+
+  //   const handleLogOut = () => {
+  //     logOut()
+  //       .then((res) => console.log(res))
+  //       .catch((err) => console.log(err));
+  //   };
+  const links = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#FA2A3B] font-semibold underline"
+              : "text-[#FA2A3B] hover:underline"
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/coverage"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#FA2A3B] font-semibold underline"
+              : "text-[#FA2A3B] hover:underline"
+          }
+        >
+          Coverage
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/sendParcel"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#FA2A3B] font-semibold underline"
+              : "text-[#FA2A3B] hover:underline"
+          }
+        >
+          Send Parcel
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/beArider"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#FA2A3B] font-semibold underline"
+              : "text-[#FA2A3B] hover:underline"
+          }
+        >
+          Be a rider
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar max-w-7xl mx-auto bg-base-100 text-[#FA2A3B] ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 text-[#FA2A3B] rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <div className="flex  items-center">
+          <Link
+            to="/"
+            className=" font-josefin font-semibold normal-case text-2xl"
+          >
+            <p className="text-3xl font-semibold">potheGo</p>
+          </Link>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        {/* <Link className="mr-3 underline text-[#4167F0]" to="/register">
+          Register
+        </Link> */}
+        {/* {user ? (
+          <button onClick={handleLogOut} className="btn bg-[#CAEB66] text-black">Log Out</button>
+        ) : (
+          <Link to="/signin">
+            <button className="btn bg-[#CAEB66] text-black">Log In</button>
+          </Link>
+        )} */}
+
+        <Link to="/signin">
+          <button className="btn bg-[#FA2A3B] ml-2 text-white">Log In</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { router } from "./Routes/Routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div>
+    <div className="max-w-[1650px] mx-auto">
     <RouterProvider router={router} />
     </div>
   </StrictMode>
