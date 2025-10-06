debd0b491407af9743a68c7612055791c5d4696e
abrarasif11 <faasif929@gmail.com>
2025-10-06 22:38:12 +0600

    setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import AuthProvider from "./context/AuthContext/AuthProvider.jsx";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className=" font-poppins max-w-[1650px] mx-auto">
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </StrictMode>
 );
