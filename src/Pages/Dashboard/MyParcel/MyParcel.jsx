242889476ede0f7dc222d83ee6c50c0952cc63be
abrarasif11 <faasif929@gmail.com>
2025-10-06 22:55:32 +0600

    show parcels length

--- src/Pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/Pages/Dashboard/MyParcel/MyParcel.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyParcel = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["my-parcels", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(parcels);
   return (
     <div>
-      <p>sasas</p>
+      <p>Total Parcels : {parcels.length}</p>
     </div>
-  )
-}
+  );
+};
 
-export default MyParcel
+export default MyParcel;
