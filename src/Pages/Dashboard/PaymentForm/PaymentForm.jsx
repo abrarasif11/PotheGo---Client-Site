c586ab9803b86a90c8483ae404b2265969bd2bfb
abrarasif11 <faasif929@gmail.com>
2025-10-08 17:13:51 +0600

    payment succesfull

--- src/Pages/Dashboard/PaymentForm/PaymentForm.jsx
+++ src/Pages/Dashboard/PaymentForm/PaymentForm.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Loader from "../../../Shared/Loader/Loader";
+import useAuth from "../../../hooks/useAuth";
 
 const PaymentForm = () => {
   const { id } = useParams();
@@ -11,6 +12,7 @@ const PaymentForm = () => {
   const elements = useElements();
   const [error, setError] = useState("");
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
 
   const { isPending, data: parcelInfo = {} } = useQuery({
     queryKey: ["parcels", id],
@@ -52,32 +54,49 @@ const PaymentForm = () => {
     } else {
       setError("");
       console.log("Payment Method", paymentMethod);
-    }
 
-    //Create Payment intent//
-    const res = await axiosSecure.post("create-payment-intent", {
-      amountInCents,
-      id,
-    });
-    const clientSecret = res.data.clientSecret;
+      // Create Payment intent //
+      const res = await axiosSecure.post("create-payment-intent", {
+        amountInCents,
+        id,
+      });
+      const clientSecret = res.data.clientSecret;
 
-    const result = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: elements.getElement(CardElement),
-        billing_details: {
-          name: "Yennefer",
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: user.displayName,
+            email: user.email,
+          },
         },
-      },
-    });
-    if (result.error) {
-      console.log(result.error.message);
-    } else {
-      if (result.paymentIntent.status === "succeeded") {
-        console.log("Payment Success!!!");
-        console.log(result);
+      });
+
+      if (result.error) {
+        setError(result.error.message);
+      } else {
+        setError("");
+        if (result.paymentIntent.status === "succeeded") {
+          console.log("Payment Success!!!");
+          console.log(result);
+
+          // Create Payment History //
+          const paymentRes = await axiosSecure.post("/payments", {
+            parcelId: id,
+            email: user?.email,
+            amount,
+            transactionId: result.paymentIntent.id,
+            paymentMethod: result.paymentIntent.payment_method_types,
+          });
+
+          if (paymentRes.data.insertedId) {
+            console.log("Payment Successful");
+          }
+        }
       }
     }
   };
+
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-md mx-auto p-4">
       <CardElement className="border p-2 rounded" />
