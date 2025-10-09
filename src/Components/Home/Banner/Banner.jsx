8bd8213e0b013602cc8b68c2a1a2a653326f1958
abrarasif11 <faasif929@gmail.com>
2025-10-09 18:19:10 +0600

    fix banner route

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import banner1 from "../../../assets/assests/banner (1).jpg";
-import banner2 from "../../../assets/assests/banner (2).jpg";
-import banner3 from "../../../assets/assests/banner (3).jpg";
+import banner1 from "../../../assets/banner/banner (1).jpg";
+import banner2 from "../../../assets/banner/banner (2).jpg";
+import banner3 from "../../../assets/banner/banner (3).jpg";
 const Banner = () => {
   return (
     <div className="mt-20 mb-20">
