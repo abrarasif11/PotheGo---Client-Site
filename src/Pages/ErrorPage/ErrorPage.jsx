85d5f328ec81dc28370fb2d121dc2c0c2804e1ae
abrarasif11 <faasif929@gmail.com>
2025-10-11 21:12:29 +0600

    error page complete

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import error from "../../assets/banner/18499954_bubble_gum200_89 1.png";
+
+const MotionImage = motion.img;
 
 const ErrorPage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col items-center rounded-3xl mb-20 mt-20 justify-center pt-20 pb-20 bg-gray-50 px-4">
+      {/* Animated Image */}
+      <MotionImage
+        src={error}
+        alt="Error 404"
+        className="w-[300px]"
+        animate={{ y: [0, -20, 0] }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      />
 
-export default ErrorPage
+      {/* Button */}
+      <button
+        onClick={() => (window.location.href = "/")}
+        className="mt-6 px-6 py-2 text-white bg-[#FA2A3B] hover:bg-[#E02032] rounded-lg shadow"
+      >
+        Go Home
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
