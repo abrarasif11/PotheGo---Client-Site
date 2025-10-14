8d072222153a29b4294bdffc7e2153be9e105b65
abrarasif11 <faasif929@gmail.com>
2025-10-14 21:56:57 +0600

    useTrackingLogger hook

---
+++ src/hooks/useTrackingLogger.jsx
@@ -0,0 +1,30 @@
+import useAxiosSecure from "./useAxiosSecure";
+
+const useTrackingLogger = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const logTracking = async ({
+    trackingId,
+    status,
+    details,
+    deliveryInstruction,
+    updatedBy,
+  }) => {
+    try {
+      const payload = {
+        tracking_id: trackingId,
+        status,
+        details,
+        deliveryInstruction,
+        updated_by: updatedBy,
+      };
+      await axiosSecure.post("trackings", payload);
+    } catch (error) {
+      console.error("Failed to log tracking:", error);
+    }
+  };
+
+  return { logTracking };
+};
+
+export default useTrackingLogger;
