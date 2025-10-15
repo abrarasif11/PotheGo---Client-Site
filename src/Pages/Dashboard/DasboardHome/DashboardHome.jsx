1f78908dddc41944ae4e318292b014f3dd79460a
abrarasif11 <faasif929@gmail.com>
2025-10-15 20:49:41 +0600

    create dashboard home

---
+++ src/Pages/Dashboard/DasboardHome/DashboardHome.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import useUserRole from "../../../hooks/useUserRole";
+import Loader from "../../../Shared/Loader/Loader";
+import UserDashboard from "./UserDashboard";
+import RiderDashboard from "./RiderDashbaord";
+import AdminDashboard from "./AdminDashboard";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+
+const DashboardHome = () => {
+  const { role, roleLoading } = useUserRole();
+
+  if (roleLoading) {
+    return <Loader />;
+  }
+  if (role === "user") {
+    return <UserDashboard />;
+  } else if (role === "rider") return <RiderDashboard />;
+  else if (role === "admin") {
+    return <AdminDashboard />;
+  } else return <ErrorPage />;
+};
+export default DashboardHome;
