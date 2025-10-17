3f6d733cc0503d3c7b82d2c4bff16480ae857c68
abrarasif11 <faasif929@gmail.com>
2025-10-17 18:13:29 +0600

    margin alignment

--- src/Pages/Dashboard/AssignRider/AssignRider.jsx
+++ src/Pages/Dashboard/AssignRider/AssignRider.jsx
@@ -99,7 +99,7 @@ const AssignRider = () => {
   return (
     <div className="max-w-7xl mx-auto p-4">
       <div className="flex items-center justify-center mb-8">
-        <h1 className="text-3xl sm:text-4xl font-bold flex items-center gap-3 text-[#FA2A3B]">
+        <h1 className="text-3xl sm:text-4xl font-bold mt-5 flex items-center gap-3 text-[#FA2A3B]">
           <FaUserPlus className="text-[#E02032]" /> Assign Rider to Parcels
         </h1>
       </div>
